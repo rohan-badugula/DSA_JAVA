e0cc2f885a6e50aea2780de82cc1a3f874f00723
rohan-badugula <rohanbadugula@gmail.com>
2024-02-27 16:27:54 +0530

    Patter printing

---
+++ Java_basics/Pattern2.java
@@ -0,0 +1,15 @@
+// inverted half pyramid
+
+import java.util.Scanner;
+public class Pattern2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i = num ; i >= 1 ; i--){
+            for(int j = 1; j <= i ; j++){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
