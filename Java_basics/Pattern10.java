d68456033387dc5f5ec07961d742cd587f37c772
rohan-badugula <rohanbadugula@gmail.com>
2024-02-28 18:51:44 +0530

    small_change

--- Java_basics/Pattern10.java
+++ Java_basics/Pattern10.java
@@ -10,7 +10,7 @@ public class Pattern10{
             for(int j = 1; j <= num; j++){
                 if(i<=num/2){
                     if(j == 1 || j == num){
-                        System.out.print(" * ");
+                        System.out.print(" *");
                     }
                     else if(j<=i || j >= (num-i+1)){
                         System.out.print(" * ");
